cbf6186c3190da2303d289654bbbabb1d0161f22
wangshanqiang <804435152@qq.com>
2014-09-15 11:25:39 +0800

    测试下咯

---
+++ src/com/cx/test/Hello.java
@@ -0,0 +1,18 @@
+package com.cx.test;
+
+/**
+ * 
+* Project:                    git_test                                         
+* Comments:                   测试git                                          
+* JDK version used:           <JDK1.7>                                                          
+* Author：                                                              wangsq                
+* Create Date：                                               2014年9月15日
+* Modified By：                                               <修改人中文名或拼音缩写>                                        
+* Modified Date:       		  <修改日期，格式:YYYY-MM-DD>                                   
+    * Why & What is modified  <修改原因描述>   
+* Version:                    1.0
+ */
+public class Hello {
+	
+	
+}
